e5b2236417ba2c5652391673e5704836533e7209
kylekaminski26 <38478613+kylekaminski26@users.noreply.github.com>
2019-10-03 21:40:57 -0400

    Add files via upload

---
+++ kaminski-assignment13/js/main.js
@@ -0,0 +1,7 @@
+let date = new Date();
+alert("Today's date and time are " + date); // Prints date message upon visiting.
+alert("My name is Kyle Kaminski. Welcome to my webpage!"); // Prints hello message.
+
+$(function () { // Initializes tooltip component
+  $('[data-toggle="tooltip"]').tooltip()
+})
