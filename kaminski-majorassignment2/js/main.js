e779e83e94e3adfeb9de56907a244a6c4b79d804
kylekaminski26 <38478613+kylekaminski26@users.noreply.github.com>
2019-10-03 21:41:41 -0400

    Add files via upload

---
+++ kaminski-majorassignment2/js/main.js
@@ -0,0 +1,5 @@
+alert("My name is Kyle Kaminski. This is my Final Project. Welcome to my webpage!"); // Prints hello message.
+
+$(function () { // Initializes tooltip component
+  $('[data-toggle="tooltip"]').tooltip()
+})
